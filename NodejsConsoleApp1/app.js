e834022f32e8ec45c01aabda48f789e9427b4427
777 <777@DESKTOP-MENSLGK>
2021-06-11 17:02:37 +0300

    Добавьте файлы проекта.

---
+++ NodejsConsoleApp1/app.js
@@ -0,0 +1,9 @@
+
+    const createPerson = (name, age, gender) => {
+        return {
+        name: name,
+            age: age,
+            gender: gender
+        };
+    };
+console.log(createPerson("Elgar", 20, "pidor"));
